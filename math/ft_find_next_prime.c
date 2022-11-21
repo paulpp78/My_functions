a331fbd3c942196969f28a11b265d339dc766d73
paulpp78 <perigault.paul@gmail.com>
2022-11-21 23:07:10 +0100

    new commit

---
+++ math/ft_find_next_prime.c
@@ -0,0 +1,11 @@
+int		ft_find_next_prime(int nb)
+{
+    int		i;
+
+    i = 0;
+    while (!ft_is_prime(nb + i))
+    {
+        i++;
+    }
+    return (nb + i);
+}
---
+++ math/ft_iterative_factorial.c
@@ -0,0 +1,18 @@
+int ft_iterative_factorial(int nb)
+{
+    int a;
+    int res;
+
+    a = 1;
+    res = 1;
+    if ((nb <= 0) || (nb > 12))
+        return (0);
+    if (nb == 1)
+        return (1);
+    while (a <= nb)
+    {
+        res *= a;
+        a++;
+    }
+    return (res);
+}
